eb30dd6cbeee77ba76dcdf377cb10ff253550913
voruti <45130096+voruti@users.noreply.github.com>
2022-10-20 23:16:14 +0200

    Code logic

--- src/main/java/voruti/velocityplayerlistquery/VelocityPlayerListQuery.java
+++ src/main/java/voruti/velocityplayerlistquery/VelocityPlayerListQuery.java
@@ -1,10 +1,17 @@
 package voruti.velocityplayerlistquery;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.EventTask;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerPing;
 import org.slf4j.Logger;
+
+import java.util.Collection;
 
 @Plugin(
         id = "velocityplayerlistquery",
@@ -19,7 +26,29 @@ public class VelocityPlayerListQuery {
     @Inject
     private Logger logger;
 
+    @Inject
+    private ProxyServer server;
+
+
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        logger.info("Enabled");
+    }
+
+    @Subscribe
+    public EventTask onServerPing(ProxyPingEvent event) {
+        return EventTask.async(() -> {
+            Collection<Player> players = server.getAllPlayers();
+
+            if (!players.isEmpty()) {
+                event.setPing(event.getPing().asBuilder()
+                        .samplePlayers(
+                                players.stream()
+                                        .map(player -> new ServerPing.SamplePlayer(player.getGameProfile().getName(),
+                                                player.getUniqueId()))
+                                        .toArray(ServerPing.SamplePlayer[]::new))
+                        .build());
+            }
+        });
     }
 }
